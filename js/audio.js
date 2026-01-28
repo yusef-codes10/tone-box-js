8a858aa3a11a95ed049d6d7c4d9bab4db5893dc3
Youcef <youcefcopy@gmail.com>
2026-01-28 19:03:48 +0100

    musoc obj have been created

--- js/audio.js
+++ js/audio.js
@@ -1,1 +1,19 @@
 // creating the music audio obj here
+const musicList = [
+    {
+        id: 'armor-hero',
+        title: 'Armor Hero',
+        music: Audio('/music/armor-hero.mp3')
+
+    }, 
+    {
+        id: 'rooftop',
+        title: 'Rooftop',
+        music: Audio('/music/rooftop.mp3')
+    }, 
+    {
+        id: 'orleans',
+        title: 'Orleans',
+        music: Audio('/music/orleans-theme.ogg')
+    }
+]
