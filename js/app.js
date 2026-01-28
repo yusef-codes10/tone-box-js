beecab762f5bb9e44c34abb671661a6615d9745b
Youcef <youcefcopy@gmail.com>
2026-01-28 18:11:26 +0100

    never trust e.target and use this procedure on event delegation

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
 console.log('app running...');
+
+// we should use event delegation for the buttons
+// const toneBoxPlayer = document.querySelector();
+const btnSection = document.querySelector('.btn-section');
+
+btnSection.addEventListener('click', e => {
+    const btn = e.target.closest('.btn');
+
+    if(!btn) return; // you did not click on the btn
+    console.log(e.target);
+})
