88d69b0705914a7e1ddd09050a8505fd29b86a34
Youcef <youcefcopy@gmail.com>
2026-01-29 10:09:51 +0100

    setting up the imgs

--- js/audio.js
+++ js/audio.js
@@ -3,18 +3,21 @@ const musicList = [
     {
         id: 'armor-hero',
         title: 'Armor Hero',
-        music: new Audio('/music/armor-hero.mp3')
+        music: new Audio('/music/armor-hero.mp3'),
+        img: '/imgs/1.jpeg'
 
     }, 
     {
         id: 'rooftop',
         title: 'Rooftop',
-        music: new Audio('/music/rooftop.mp3')
+        music: new Audio('/music/rooftop.mp3'),
+        img: '/imgs/2.jpeg'
     }, 
     {
         id: 'orleans',
         title: 'Orleans',
-        music: new Audio('/music/orleans-theme.ogg')
+        music: new Audio('/music/orleans-theme.ogg'),
+        img: '/imgs/3.jpeg'
     }
 ]
 
--- js/disc.js
+++ js/disc.js
@@ -1,2 +1,10 @@
+import { musicList } from "./audio.js";
+
 // here we are going to load the imgs into disc-container
 const discContainer = document.querySelector('.disc-container');
+
+musicList.forEach(
+    image => {
+        
+    }
+)
