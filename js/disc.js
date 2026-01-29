9e2e753e5e2d3eff9776663184b69f2f8a94de59
Youcef <youcefcopy@gmail.com>
2026-01-29 10:19:55 +0100

    loadImg() logic

--- js/disc.js
+++ js/disc.js
@@ -3,8 +3,15 @@ import { musicList } from "./audio.js";
 // here we are going to load the imgs into disc-container
 const discContainer = document.querySelector('.disc-container');
 
-musicList.forEach(
-    image => {
-        
-    }
-)
+function loadImg() {
+    musicList.forEach(
+        image => {
+            const img = document.createElement('img');
+            img.src = musicList[image].img;
+            img.alt = musicList[image].id;
+
+            discContainer.appendChild(img);
+        }
+    )
+}
+export {loadImg};
